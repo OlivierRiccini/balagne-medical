4141deee56ce42a79446f4c8c5fe4a4b6cdf100c
Olivier Riccini <olivier-webdev@hotmail.com>
2019-08-17 19:14:35 -0400

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { IndividualsComponent } from './individuals/individuals.component';
+import { ProfessionalsComponent } from './professionals/professionals.component';
+import { PharmacistsComponent } from './pharmacists/pharmacists.component';
+import { ContactComponent } from './contact/contact.component';
+import { TermsOfUseComponent } from './terms-of-use/terms-of-use.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  component: HomeComponent },
+  { path: 'services-particuliers',  component: IndividualsComponent },
+  { path: 'services-professionnels',  component: ProfessionalsComponent },
+  { path: 'catalogue-pharmacies',  component: PharmacistsComponent },
+  { path: 'contact',  component: ContactComponent },
+  { path: 'mentions-legales',  component: TermsOfUseComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { IndividualsComponent } from './individuals/individuals.component';
+import { ProfessionalsComponent } from './professionals/professionals.component';
+import { PharmacistsComponent } from './pharmacists/pharmacists.component';
+import { ContactComponent } from './contact/contact.component';
+import { TermsOfUseComponent } from './terms-of-use/terms-of-use.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IndividualsComponent,
+    ProfessionalsComponent,
+    PharmacistsComponent,
+    ContactComponent,
+    TermsOfUseComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
