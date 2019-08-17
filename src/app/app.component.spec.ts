cf0e2c9610890d16fa94a6224301bf7a8d6509d5
Olivier Riccini <olivier-webdev@hotmail.com>
2019-08-17 18:47:45 -0400

    update name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jjo-showcase'`, () => {
+  it(`should have as title balagne-medical`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('jjo-showcase');
+    expect(app.title).toEqual('balagne-medical');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to jjo-showcase!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to balagne-medical!');
   });
 });
