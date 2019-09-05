69e2aa19d278ea7f6b2906abccd5d687bc52adee
Olivier Riccini <olivier-webdev@hotmail.com>
2019-09-05 18:44:09 -0400

    add interfaces for content

---
+++ src/app/models/content.ts
@@ -0,0 +1,17 @@
+export interface IContentSection { 
+    title: { text: string, styleOptions: string[] },
+    paragraphes: { text: string, styleOptions: string[] }[],
+    images: { url: string, styleOptions: string[] },
+    styleOptions: string[]
+}
+
+export interface IContentChapter {
+    title: { text: string, styleOptions: string[] },
+    sections: IContentSection[],
+}
+
+export interface IContent {
+    [page: string]: {
+        chapter: IContentChapter[]
+    }
+}
