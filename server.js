f08c641625bd31dc1d96c05ebac956e3c03765a7
Olivier Riccini <olivier-webdev@hotmail.com>
2019-09-17 00:47:49 -0400

    try

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const fs = require("fs");
-
-const appName = process.env.HEROKU_APP_NAME;
-if (appName) {
-  const data = fs.readFileSync('./src/environments/environment.prod.ts', 'utf-8');
-  const appURL = `URL: 'https://${appName}.herokuapp.com',`;
-  const newValue = data.replace(/URL:(.*)/g, appURL);
-  fs.writeFileSync('./src/environments/environment.prod.ts', newValue, 'utf-8');
-}
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/balagne-medical'));
